6829594e1af013e67fddd738690e332b4b1051f4
Rajiv Ranjan <rjchamps@gmail.com>
2025-01-13 15:37:09 -0500

    changes

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "ML-pipeline-using-Gitops"
+AUTHOR_USER_NAME = "rajivgmai"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "rjchamps@gmail.com"
 
 
 setuptools.setup(
